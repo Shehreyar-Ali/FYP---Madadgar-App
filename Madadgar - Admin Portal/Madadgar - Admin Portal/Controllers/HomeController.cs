b5599250c827d0c8f12b7a02fa3ad3affa415f1e
sa03576 <31242894+sa03576@users.noreply.github.com>
2020-12-04 22:36:58 +0500

    Update
    Usless views removed, login view and model added, model is connected to an SQL server so if any queries ask Shehreyar on how to connect it,
    
    Stuff left:
    second screen after login, beautification

--- Madadgar - Admin Portal/Madadgar - Admin Portal/Controllers/HomeController.cs
+++ Madadgar - Admin Portal/Madadgar - Admin Portal/Controllers/HomeController.cs
@@ -1,30 +1,53 @@
-﻿using System;
+﻿using Madadgar___Admin_Portal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Madadgar___Admin_Portal.Controllers
 {
     public class HomeController : Controller
     {
+
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Admin Login Page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Login(Admin model, string returnUrl)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            Entities db = new Entities();
+            //var dataItem = db.Admins.Where()
+            var dataItem = db.Admins.Where(x => x.username == model.username && x.password_2 == model.password_2).First();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.username, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                         && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid user/pass");
+                return View();
+            }
         }
     }
 }
